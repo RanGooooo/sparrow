6fcdd033cef377924a5ce00488e799a90aafaf08
RanGooooo <13835218803@163.com>
2019-11-02 07:12:19 +0800

    静态文件

--- src/main/java/com/rain/sparrow/system/login/LoginController.java
+++ src/main/java/com/rain/sparrow/system/login/LoginController.java
@@ -1,4 +1,17 @@
 package com.rain.sparrow.system.login;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("loginController")
 public class LoginController {
+
+    @RequestMapping("login")
+    public String login(){
+        return "system/login";
+    }
+
+
 }

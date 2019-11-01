b1ae00ca03873f3c434aa14d4238205b14b0934f
RanGooooo <13835218803@163.com>
2019-11-02 06:43:10 +0800

    Initial commit

---
+++ src/test/java/com/rain/sparrow/SparrowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rain.sparrow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparrowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
